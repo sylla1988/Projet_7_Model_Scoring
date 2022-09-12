dc9c7ea1b71af5fa86a936ca91ddaef8ef6faaf0
SYLLA1988 <syllamahamadou22@yahoo.fr>
2022-09-12 23:00:56 +0200

    Ajout des fonctions des rappelle pour mise à jour des onglets

--- dash_projet7.py
+++ dash_projet7.py
@@ -511,3 +511,241 @@ html.Div([
         ]) #fin div global
 
 ]) #fin div Layout de app dash
+# Création d'un système de filtre
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+def split_filter_part(filter_part):
+    # Permet d'avoir un outil de filtrage des données
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+############################ infos client #######################################################""
+
+@app.callback([Output('table_infos', 'data'),Output('table_infos', 'columns')],
+              [Input("id_client", 'value')]) #obliger de la mettre entre crocher meme si c"est un seul elemen
+def infos_client(id_client):
+    infos_list = ['NAME_CONTRACT_TYPE','CODE_GENDER','FLAG_OWN_CAR','FLAG_OWN_REALTY', 'NAME_INCOME_TYPE',
+                  'NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS',"REG_CITY_NOT_LIVE_CITY",'REG_REGION_NOT_LIVE_REGION',
+                'REG_REGION_NOT_WORK_REGION', 'REG_CITY_NOT_WORK_CITY','LIVE_REGION_NOT_WORK_REGION','CNT_FAM_MEMBERS',
+              'LIVE_CITY_NOT_WORK_CITY', 'AMT_INCOME_TOTAL']
+    
+    #info_list = [info  for info in col ]
+    infos = {'intitule':infos_list,
+            'donnee': [app_test[app_test.index==id_client][col].iloc[0] for col in infos_list]
+            }
+    
+    table = pd.DataFrame(infos)
+    data = table.to_dict('rows')
+    entete = {'id':'intitule', 'name': "Principaux indicateurs"}, {'id': 'donnee',
+                                                                   'name': "Id_client: " + str(id_client)}
+    
+    return data, entete
+    #--------------------------------------- grahe pie client----------------------------------------------------#
+@app.callback(Output('bar_infos', 'figure'),
+              [Input('id_client', 'value')])
+def graph_bar_infos_client(id_client):
+    
+    col_bar = ['AMT_INCOME_TOTAL','AMT_CREDIT','AMT_GOODS_PRICE']
+    data_info = [float(app_test[app_test.index ==id_client][col].iloc[0]) for col in col_bar]
+    
+    #data_moy_age_pop = round(app_test["DAYS_BIRTH"].mean(), 0).astype(int)
+    data_moy_incom_pop = round(app_test["AMT_INCOME_TOTAL"].mean(), 2)
+    data_moy_credit_pop = round(app_test["AMT_CREDIT"].mean(),2)
+    data_moy_goods_pop = round(app_test["AMT_GOODS_PRICE"].mean(), 2)
+    
+    data_pop= [data_moy_incom_pop, data_moy_credit_pop,  data_moy_goods_pop]
+    
+    labels =  col_bar
+   
+    traces = [
+        go.Bar(x= labels, y =data_info, text =data_info , textposition='auto', name = "Client choisi"),
+       go.Bar(x= labels, y = data_pop, text =data_pop , textposition='auto', name = "Moyennne Pop"  ),
+
+    ]
+    del data_info
+    return {
+        'data': traces,
+        'layout': go.Layout(
+           
+            title= 'Information client vs moyenne de la population <br> dans la même catégorie',
+            legend_orientation= 'h'
+        )
+    }
+############################################## acceptabilité de la demande ##################""
+@app.callback(
+    Output('result', 'children'),
+    [Input('id_client', 'value')])
+
+def update_result(id_client):
+    proba = pipe_model[3].predict_proba(np.array(x_test_transformed.loc[id_client]).reshape(1, -1)).flatten()
+    proba = proba[1]
+    if (proba < 0.541237): 
+        return "Demande de prêt accéptée !"
+    else:
+        return "Demande de prêt refusée !"
+
+
+############################ probabilté de faillite #######################################################""
+
+@app.callback(Output('proba_value', 'figure'),
+             [Input('id_client', 'value')])
+def update_proba_bar(id_client):    
+    proba = pipe_model[3].predict_proba(np.array(x_test_transformed.loc[id_client]).reshape(1, -1)).flatten()
+    prod = np.array([proba[1]])
+    df = ["Classe"]
+    fig =px.bar(x = df, y= prod,
+                labels=dict(x = '...= Seuil', y='Score proba'),
+               title='Le score de probabilité  pour prise de décision ', text_auto=True)
+    fig.add_hline(y=0.8, line_color="black", line_dash="dot", line_width = 0.001)
+    fig.add_hline(y=0.54123, line_color="red", line_dash="dot", line_width = 2)
+    return fig
+    del proba
+#****************************************graph de lime**********************************
+@app.callback(Output('graph_lime', 'figure'),
+             [Input('id_client', 'value')])
+def graphe_lime(id_client) :
+     
+    exp = explainer.explain_instance(x_test_transformed.loc[id_client],
+                                 pipe_model[3].predict_proba)
+    
+    indices, values = [], []
+    
+
+    for ind, val in sorted(exp.as_list(), key=itemgetter(1)):
+        indices.append(ind)
+        values.append(val)
+    data = pd.DataFrame(values, columns=["values"], index=indices)
+    data["positive"] = data["values"]>0
+    del indices, values
+    
+    # Retourne le barplot correspondant aux 'feature importances'
+    # du client dont l'id est séléctionné sur le dashboard
+    traces = [go.Bar(
+                    x=data["values"],
+                    y=data.index,
+                    orientation='h',
+                    marker_color=list(data.positive.map({True: 'red', False: 'green'}).values)
+        )]
+    return {
+        
+        'data': traces,
+        
+        'layout': go.Layout(
+                            margin=dict(l=300, r=0, t=30, b=100),
+            title = "Influences des variables(Vert facilitant Non faillite et rouge  faillite)"
+        )  
+                           
+    }
+############################################ client similaire ################################
+@app.callback(
+    Output('client_similaire', 'data'),
+    [Input('client_similaire', "filter_query"),
+     Input('id_client', "value")])
+def update_table(filter, id_client):
+    
+    # Déterminer les individus les plus proches du client dont l'id est séléctionné
+    indices_similary_clients = nbrs.kneighbors(np.array(x_test_transformed.loc[id_client]).reshape(1, -1))[1].flatten()
+     
+    filtering_expressions = filter.split(' && ')
+    dff = app_test.iloc[indices_similary_clients].reset_index()
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+    
+    return dff.to_dict('records')
+   ################################### graphe_comp_1 ###################### 
+@app.callback(Output('graph_comp_1', 'figure'),
+             [Input('id_client', 'value'), Input('variable', 'value')])
+def graph_comparaison_1(id_client,variable):
+    indices_similary_clients = nbrs.kneighbors(np.array(x_test_transformed.loc[id_client]).reshape(1, -1))[1].flatten()
+    df_client_simil = app_test.iloc[indices_similary_clients]
+ 
+    ind_client_vs = df_client_simil.index.to_list()
+    
+    data_client_princi = df_client_simil[df_client_simil.index == id_client][variable].iloc[0]
+    data_client_1 = df_client_simil[df_client_simil.index  == ind_client_vs[1]][variable].iloc[0]
+    data_client_2 = df_client_simil[df_client_simil.index  == ind_client_vs[2]][variable].iloc[0]
+    data_client_3 = df_client_simil[df_client_simil.index  == ind_client_vs[3]][variable].iloc[0]
+    data_client_4 = df_client_simil[df_client_simil.index  == ind_client_vs[4]][variable].iloc[0]
+
+    labels = ['Client choisi', 'Voisin_1', "Voisin_2", "Voisin_3", "Voisin_4"]
+    values = [float(data_client_princi), float(data_client_1),float(data_client_2), float(data_client_3),
+              float( data_client_4) ]
+    
+    total = sum(values)
+
+    traces = [
+        go.Pie(labels= labels, values= values, texttemplate = "%{label}: %{value:s} <br>(%{percent})",
+    textposition = "inside")
+    ]
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+        title= " Information du client choisi comparée <br> à celle de ses 4 + proches voisins  (Total: " + str(total) + ")",
+            #legend_orientation= 'h'
+        )
+    }
+##################################################Graphe moyenne #####################################################
+@app.callback(Output('bar_moyenne', 'figure'),
+             [Input('id_client', 'value'), Input('variable_info', 'value')])
+def graph_bar_moyenne(id_client,variable_info):
+
+    
+    data_client_princi = app_test[app_test.index == id_client][variable_info].iloc[0]
+    data_moy = round(app_test[variable_info].mean(), 0).astype(int)
+ 
+
+    labels = ['Client choisi', "Moy_Pop"]
+    values = [float(data_client_princi), float(data_moy)]
+    
+    total = sum(values)
+
+    traces = [
+        go.Pie(labels= labels, values= values, hole =0.5, texttemplate = "%{label}: %{value:s} <br>(%{percent})",
+    textposition = "inside")
+    ]
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+        title= " Information du client choisi comparée <br> à la moyenne de la population (Total: " + str(total) + ")",
+            #legend_orientation= 'h'
+        )
+    }
+server =  app.server  
+if __name__ == "__main__":
+    app.run_server(debug =True) 
